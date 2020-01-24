fd4db18fb6e48cc17d557a9404e9ac2bde37c9b5
Varinder singh <varinder83singh@gmail.com>
2020-01-23 21:37:53 -0800

    final build

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,62 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
+
+const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
+
+  const { _id, name, email, phone, type } = contact;
+  let resType;
+  type === 'professional' ? (resType = 'dinner') : (resType = 'brunch');
+  const onDelete = () => {
+    deleteContact(_id);
+    clearCurrent();
+  };
+
+  return (
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
+        {name}{' '}
+        <span
+          style={{ float: 'right' }}
+          className={
+            'badge ' +
+            (type === 'professional' ? 'badge-success' : 'badge-primary')
+          }
+        >
+          {resType.charAt(0).toUpperCase() + resType.slice(1)}
+        </span>
+      </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-city' /> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-sticky-note' /> {phone}
+          </li>
+        )}
+      </ul>
+      <p>
+        <button
+          className='btn btn-dark btn-sm'
+          onClick={() => setCurrent(contact)}
+        >
+          Edit
+        </button>
+        <button className='btn btn-danger btn-sm' onClick={onDelete}>
+          Delete
+        </button>
+      </p>
+    </div>
+  );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default ContactItem;
